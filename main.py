2a989a5a17b52b6125783e11149fce28e47996a5
SiyiALI <129675841+SiyiALI@users.noreply.github.com>
2025-10-13 09:17:01 +0200

    write output in file

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ def tp():
   z = random.random()
   if (x+y)+z!=x+(y+z):
   #if abs((x+y)+z)-(x+(y+z)) >= 0.000000000000001:
-    print(f"False: {x}, {y}, {z}")
+    #print(f"False: {x}, {y}, {z}")
     t=t+1
 
 for i in range(1000):
   tp()
 
-print({1-t/1000})
+with open("answer_associativity.txt", "w+") as file:
+  file.write(str(1-t/1000))
+
+#print({1-t/1000})
